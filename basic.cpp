48a1243b281dea1ddf858ea9efaee4022fc45095
NIKHIL <63747907+nikhiljha261@users.noreply.github.com>
2021-02-09 23:31:50 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,35 @@
+//har har mahadev
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define loop(i,a,b)     for(int i=(a);i<(b);i++)
+#define endl            "\n"
+
+void ganeshji(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main()
+{
+	ganeshji();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    clock_t begin = clock();
+    ll t=1;
+    // cin>>t;
+    while(t--)
+    {
+        
+        
+    }
+    #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif
+}
+
